fcb5da9dc0f15c8b2f24fb2791b8e05d942a1667
Yuri Sousa Almeida <127100378+YuriSousaAlmeida@users.noreply.github.com>
2026-04-28 03:29:42 -0300

    Add files via upload

--- Header.h
+++ Header.h
@@ -1,10 +1,20 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 #define MAX_CIDADE 200
+#define alfa 100
 
 int num_cidade;
 float mat_custo[MAX_CIDADE][MAX_CIDADE];
 
+typedef struct Tsolucao{
+    float fo;
+    int cidadesVisitadas[MAX_CIDADE];
+    int qtdVisitas[MAX_CIDADE];
+}Solucao;
+
 void ler_instancia(char *arq);
-void imprimirDadosIniciais(int *inteiro);
+void imprimirDadosIniciais(int inteiro, float matriz[MAX_CIDADE][MAX_CIDADE]);
+void imprimirNaTela(Solucao &sol);
+void imprimirNoArquivo(Solucao &sol, char *nomeArq);
+void calcularFo(Solucao &sol);
 #endif
--- main.cpp
+++ main.cpp
@@ -10,17 +10,30 @@ int main()
 {
     char arquivo[50];
     strcpy(arquivo, "toy.txt");
+    Solucao sol;
+    sol.fo=0;
 
     ler_instancia(arquivo);
 
-    imprimirDadosIniciais(&num_cidade);
-    //printf("%d\n", num_cidade);
+    sol.cidadesVisitadas[0]=0;
+    sol.cidadesVisitadas[1]=3;
+    sol.cidadesVisitadas[2]=4;
+    sol.cidadesVisitadas[3]=3;
+    sol.cidadesVisitadas[4]=1;
 
-    for(int i=0;i<num_cidade;i++){
-        for(int j=0;j<num_cidade;j++){
-            printf("%.2f ", mat_custo[i][j]);
-        }printf("\n");
-    }
+    sol.qtdVisitas[0]=1;
+    sol.qtdVisitas[1]=1;
+    sol.qtdVisitas[2]=0; //multa por não visitar
+    sol.qtdVisitas[3]=2; //multa por excesso de visita
+    sol.qtdVisitas[4]=1;
+
+    imprimirDadosIniciais(num_cidade,mat_custo);
+    calcularFo(sol);
+    imprimirNaTela(sol);
+    imprimirNoArquivo(sol, "SaidaTela.txt");
+
+
+
 
     return 0;
 }
@@ -39,6 +52,51 @@ void ler_instancia(char *arq){
     fclose(f);
 }
 
-void imprimirDadosIniciais(int *inteiro){
+//Para teste dos dados de entrada
+void imprimirDadosIniciais(int inteiro, float matriz[MAX_CIDADE][MAX_CIDADE]) {
     printf("%d\n", num_cidade);
+    for(int i=0;i<num_cidade;i++){
+        for(int j=0;j<num_cidade;j++){
+            printf("%.2f ", mat_custo[i][j]);
+        }printf("\n");
+    }
+    printf("\n");
+}
+
+void imprimirNaTela(Solucao &sol){
+    printf("FO: %.2f\n", sol.fo);
+    printf("Cidades percorridas: Iniciando na cidade ");
+    for(int i=0; i<num_cidade;i++){
+        printf("[->]:%d   ", sol.cidadesVisitadas[i]);
+    }printf("[->]:%d\n", sol.cidadesVisitadas[0]);
+    printf("Quantidade p/ cidade: ");
+    for(int i=0; i<num_cidade;i++){
+        printf("[%d]:%d   ",i , sol.qtdVisitas[i]);
+    }printf("\n");
+}
+
+void imprimirNoArquivo(Solucao &sol, char *nomeArq){
+    FILE *f;
+    f=fopen(nomeArq, "w");
+    fprintf(f,"FO: %.2f\n", sol.fo);
+    fprintf(f,"Cidades percorridas: Iniciando na cidade ");
+    for(int i=0; i<num_cidade;i++){
+        fprintf(f,"[->]:%d   ", sol.cidadesVisitadas[i]);
+    }fprintf(f,"[->]:%d\n", sol.cidadesVisitadas[0]);
+    fprintf(f,"Quantidade p/ cidade: ");
+    for(int i=0; i<num_cidade;i++){
+        fprintf(f,"[%d]:%d   ",i , sol.qtdVisitas[i]);
+    }fprintf(f,"\n");
+    fclose(f);
+}
+
+//automatizar o processo da contagem de cidades visitadas
+void calcularFo(Solucao &sol){
+    for(int i=0;i<num_cidade-1;i++){
+            sol.fo+=mat_custo[sol.cidadesVisitadas[i]][sol.cidadesVisitadas[i+1]];
+    }sol.fo+=mat_custo[sol.cidadesVisitadas[num_cidade-1]][sol.cidadesVisitadas[0]];
+    for(int j=0;j<num_cidade;j++){
+        if(sol.qtdVisitas[j]==0) sol.fo+=alfa;
+        else if(sol.qtdVisitas[j]>1) sol.fo+=(alfa*(sol.qtdVisitas[j]-1));
+    }
 }
